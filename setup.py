9d74723dfbb6ac4df6298e13ce350db4896d4412
David Arroyo Menéndez <davidam@gmail.com>
2021-05-31 20:54:44 +0200

    setup.py: update

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ with open(path.join(this_directory, 'README.md')) as f:
     long_description = f.read()
 
 setup(name='damedb',
-      version='0.0.5',
+      version='0.0.6',
       description='Learning Databases in Python from tests by David Arroyo MEnéndez',
       long_description= long_description,
       classifiers=[
