70d89006b2a6a141f7a8772ea3c7641fa935a94e
David Arroyo Menéndez <davidam@gmail.com>
2021-06-22 22:13:52 +0200

    tests/test_damedb_psql.py: delete comments fixing PEP8

--- damedb/tests/test_damedb_psql.py
+++ damedb/tests/test_damedb_psql.py
@@ -31,14 +31,6 @@ class TestPSQL(TestCase):
         # Open a cursor to perform database operations
         cur = conn.cursor()
 
-        # Execute a command: this creates a new table
-        #cur.execute("CREATE TABLE test (id serial PRIMARY KEY, num integer, data varchar);")
-
-        # Pass data to fill a query placeholders and let Psycopg perform
-        # the correct conversion (no more SQL injections!)
-        # cur.execute("INSERT INTO test (num, data) VALUES (%s, %s)",
-        #             (100, "abc'def"))
-
         # Query the database and obtain data as Python objects
         cur.execute("SELECT * FROM test;")
         x = cur.fetchone()
